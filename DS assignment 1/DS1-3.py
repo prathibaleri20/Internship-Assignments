f49fc424edbe773632ad20d0ebbad23c041a4c87
Prathiksha Harish Baleri <122948382+prathibaleri20@users.noreply.github.com>
2026-02-10 11:57:44 +0530

    ASSIGNMENT 1

---
+++ DS assignment 1/DS1-3.py
@@ -0,0 +1,12 @@
+#A program to accept 3 subject marks and calculate total, average and print it
+
+phy=int(input("enter physics marks"))
+mat=int(input("enter maths marks"))
+che=int(input("enter chemistry marks"))
+tot=phy+mat+che
+avg=tot/3
+print(phy)
+print(mat)
+print(che)
+print(tot)
+print(avg)
---
+++ DS assignment 1/DSL1-1.py
@@ -0,0 +1,8 @@
+#program to accept integer data from the user during runtime
+a=int(input("enter the first number"))
+b=int(input("enter second number"))
+c=a+b
+print(a)
+print(b)
+print(c)
+
