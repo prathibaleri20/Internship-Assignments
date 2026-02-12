54a9408e1515744e6fdc714ca38413697e71a443
Prathiksha Harish Baleri <122948382+prathibaleri20@users.noreply.github.com>
2026-02-13 00:46:35 +0530

    Delete DS assignment 1 directory

--- DS assignment 1/DSL1-6.py
+++
@@ -1,8 +0,0 @@
-#A program to accept any 2 names and print it
-
-fn=input("enter first name")
-ln=input("enter last name")
-c=fn + ln
-print("first name=",fn)
-print("last name=",ln)
-print("full name=",c)
