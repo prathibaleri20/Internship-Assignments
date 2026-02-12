fed903eb5a4bf8183a61038e2c46efa90b4dcaf2
Prathiksha Harish Baleri <122948382+prathibaleri20@users.noreply.github.com>
2026-02-13 00:46:46 +0530

    Delete DS assignment 2 directory

--- DS assignment 2/DS2-1.py
+++
@@ -1,8 +0,0 @@
-#Check if a person is eligible to vote or not
-age=int(input("enter any age"))
-
-if (age>=18):
-	print("eligible to vote")
-else:
-	print("not eligible to vote")
-
--- DS assignment 2/DS2-2.py
+++
@@ -1,8 +0,0 @@
-# FIND HIGHEST NUMBER
-a=int(input("enter first number"))
-b=int(input("enter second number"))
-
-if (a>b):
-	print("the first number is highest")
-else:
-	print("the second number is highest")
