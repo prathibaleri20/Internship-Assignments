dafa96fefddeaffbfc774c4f81df7d19975c6de6
Prathiksha Harish Baleri <122948382+prathibaleri20@users.noreply.github.com>
2026-02-26 00:09:53 +0530

    DS ASSIGNMENT 10

---
+++ Ds assignment 10/MATPLOTLIB.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, marker = 'o')
+plt.show()
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, 'o:r')
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, marker='o', ms=20)
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, marker='o', ms=20)
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, marker = 'o', ms = 20, mfc = 'r')
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r', mfc = 'r')
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, linestyle = 'dotted')
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, linestyle = 'dashed')
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, ls = ':')
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, color='r')
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, linewidth='20.5')
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+y1 = np.array([3, 8, 1, 10])
+y2 = np.array([6, 2, 7, 11])
+plt.plot(y1)
+plt.plot(y2)
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
+plt.plot(x1, y1, x2, y2)
+plt.show()
+
