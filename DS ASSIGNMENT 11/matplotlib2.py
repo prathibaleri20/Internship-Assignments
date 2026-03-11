7242fe5d5fe709414fa71f8ea4a17d12640d3599
Prathiksha Harish Baleri <122948382+prathibaleri20@users.noreply.github.com>
2026-03-11 18:15:40 +0530

    DS ASSIGNMENT 11

---
+++ DS ASSIGNMENT 11/matplotlib2.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+y1 = np.array([3, 8, 1, 10])
+y2 = np.array([6, 2, 7, 11])
+plt.plot(y1)
+plt.plot(y2)
+plt.show()
+
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
+plt.plot(x1, y1, x2, y2)
+plt.show()
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+plt.title("Sports Watch Data", loc = 'left')
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+plt.plot(x, y)
+plt.show()
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+plt.plot(x, y)
+plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+plt.show()
